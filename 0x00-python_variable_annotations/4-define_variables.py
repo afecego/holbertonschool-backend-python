c822aefb3339fe5ea36aa4a3511bccbda1325bdd
afecego <afecego@gmail.com>
2022-04-21 19:17:07 -0500

    Correction annotation of funtions

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """Return type of variable"""
 
 
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = str('Holberton')
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
