3b20ca50664e79b2bb9e1ab4469b9eccf7a8625a
afecego <afecego@gmail.com>
2022-04-25 19:36:30 -0500

    Correction type of return and documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """return random delay between 0 and max_delay seconds"""
     i = random.uniform(0, max_delay)
     return i
