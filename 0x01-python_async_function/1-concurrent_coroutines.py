ac0567e6cfc811441527ea1f097632eaa760fa8d
afecego <afecego@gmail.com>
2022-04-26 10:01:09 -0500

    multiples corouting at the same time

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Import wait_random from the previous python file that you’ve written and
+write an async routine called wait_n that takes in 2 int arguments"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays"""
+    delay = []
+    for _ in range(n):
+        r = await wait_random(max_delay)
+        delay.append(r)
+    return delay
